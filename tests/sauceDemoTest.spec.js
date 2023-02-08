edd58102c6a69ff873be96fdf4d1d34a54ced879
laksh <lakshminarasimhan254@gmail.com>
2023-02-08 15:37:47 -0500

    first commit

---
+++ tests/sauceDemoTest.spec.js
@@ -0,0 +1,23 @@
+import { test, expect} from '@playwright/test';
+
+test('sauceDemoTest-Desktop', async ({page}) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveTitle('Swag Labs');
+})
+
+test('sauceDemoTest-Mobile', async ({page}) => {
+   await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await browser.close;
+})
